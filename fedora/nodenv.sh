eb6a3830d6252095b4f9df7a174efc2fc55fe54d
Matheus E. Muller <hello@memuller.com>
2019-01-12 23:17:50 -0200

    Adds builder & upgrade handler

--- fedora/nodenv.sh
+++ fedora/nodenv.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-git clone https://github.com/nodenv/nodenv.git ~/.nodenv
-cd ~/.nodenv && src/configure && make -C src
+if [ ! -d "$HOME/.nodenv" ] ; then 
+  git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+  cd ~/.nodenv && src/configure && make -C src
 
-~/.nodenv/bin/nodenv init
+  ~/.nodenv/bin/nodenv init
 
-mkdir -p "$(nodenv root)"/plugins
-git clone https://github.com/nodenv/node-build.git "$(nodenv root)"/plugins/node-build
+  mkdir -p "$(nodenv root)"/plugins
+  git clone https://github.com/nodenv/node-build.git "$(nodenv root)"/plugins/node-build
+
+# if already installed, upgrades
+else
+  cd ~/.nodenv && git pull
+  cd "$(nodenv root)"/plugins/node-build && git pull
+fi
