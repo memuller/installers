0843d299830d0df665eb30c15ae8fb5c76a92d39
Matheus E. Muller <hello@memuller.com>
2019-01-14 05:05:41 -0200

    Improves font installer script

--- fedora/fonts.sh
+++ fedora/fonts.sh
@@ -3,8 +3,20 @@
 # Moves all .ttf and .otf fonts on Repos/fonts
 # to the user fonts folder, then refreshes the font cache.
 
-mkdir ~/.fonts
+# creates ~/.fonts if not exists
+[ ! -d "$HOME/.fonts" ] && mkdir ~/.fonts
+
+# goes to source fonts folder
 cd  ~/Repos/env/fonts
+
+# removes all formats expect .ttf from FiraCode repo
+rm -fr code/FiraCode/distr/eot
+rm -fr code/FiraCode/distr/otf
+rm -fr code/FiraCode/distr/woff
+rm -fr code/FiraCode/distr/woff2
+
+# copies all ttf and otf files to ~/.fonts
 find . -name "*.ttf" -or -name "*.otf" | xargs cp -t ~/.fonts/
 
-fc-cache -v
+# if fc-cache exists, reloads it
+which fc-cache >/dev/null 2>&1 && fc-cache -v
